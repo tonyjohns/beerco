cf00c0e24626f95696b6c8f6787f86d3abeff3af
Anton <tonhyjohns@gmail.com>
2023-03-11 16:41:17 +0200

    Added slider

--- main.js
+++ main.js
@@ -5,8 +5,32 @@ const hiddenElements = document.querySelectorAll('.hidden')
 showMoreButton.onclick = () => {
   hiddenElements.forEach(element=>{
     element.classList.remove("hidden");
-    element.style.opacity = 1;
   })
   showMoreButton.style.display = 'none';
   hiddenElements[hiddenElements.length - 1].scrollIntoView({ behavior: 'smooth' })
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var slides = document.querySelectorAll(".gallery-slide img");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+setInterval(function () {
+  plusSlide(1);
+}, 15000);
